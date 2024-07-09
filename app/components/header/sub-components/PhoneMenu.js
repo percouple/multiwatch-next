e45227cffe04b8e269951eacd0b67b46c3d599fe
Caleb Evans <calebsevan@gmail.com>
2024-07-09 17:47:32 -0400

    more styling

--- app/components/header/sub-components/PhoneMenu.js
+++ app/components/header/sub-components/PhoneMenu.js
@@ -1,13 +1,20 @@
 import { useDispatch } from "react-redux";
-import { setAuthenticating, setDisplayPhoneMenu, setSigningIn } from "../../state/slices/authSlice";
+import {
+  setAuthenticating,
+  setDisplayPhoneMenu,
+  setSigningIn,
+} from "../../state/slices/authSlice";
 import { nanoid } from "nanoid";
-import { addNewClock, flagForDBUpdate } from "../../state/slices/clockDataSlice";
+import {
+  addNewClock,
+  flagForDBUpdate,
+} from "../../state/slices/clockDataSlice";
 
 export default function PhoneMenu() {
   const dispatch = useDispatch();
 
   const loginHandler = (e) => {
-    if (e.target.id === "login") {
+    if (e.target.id === "menu-login") {
       console.log("CLICKING LOGIN");
       e.stopPropagation();
       dispatch(setAuthenticating(true));
@@ -17,10 +24,11 @@ export default function PhoneMenu() {
   };
 
   const themeHandler = (e) => {
-    if (e.target.id === "login") {
+    if (e.target.id === "menu-changeTheme") {
       console.log("CLICKING THEME CHANGE");
       e.stopPropagation();
       dispatch(setAuthenticating(true));
+      dispatch(setDisplayPhoneMenu(false));
       dispatch(setThemeChanging(true));
     }
   };
@@ -30,7 +38,11 @@ export default function PhoneMenu() {
       console.log("CLICKING ADD NEW CLOCK");
       e.stopPropagation();
       const clockId = nanoid();
-      const clockNeeds = { userId: "New User", name: "New Clock", clockId: clockId };
+      const clockNeeds = {
+        userId: "New User",
+        name: "New Clock",
+        clockId: clockId,
+      };
       dispatch(addNewClock(clockNeeds));
       dispatch(flagForDBUpdate());
       dispatch(setAuthenticating(false));
@@ -42,10 +54,18 @@ export default function PhoneMenu() {
       className="flex flex-col justify-around items-center p-2 pt-6 w-72 h-auto absolute rounded-lg border-4 border-accent-1 top-30% z-30 bg-bkg"
       id="phoneMenu"
     >
-      <div className="text-accent-2 text-2xl font-bold mb-4">Log in</div>
-      <div className="text-accent-2 text-2xl font-bold mb-4">Themes</div>
-      <div id="menu-addNewClock"
-        className="text-accent-2 text-2xl font-bold mb-4 cursor-pointer"
+      <div id="menu-login" className="text-accent-2 text-2xl font-bold mb-4 cursor-pointer m-2 underline">
+        Log in
+      </div>
+      <div
+        id="menu-changeTheme"
+        className="text-accent-2 text-2xl font-bold mb-4 cursor-pointer m-2 underline"
+      >
+        Themes
+      </div>
+      <div
+        id="menu-addNewClock"
+        className="text-accent-2 text-2xl font-bold mb-4 cursor-pointer m-2 underline"
         onClick={addNewClockHandler}
       >
         Create New Clock
