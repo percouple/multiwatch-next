bea609364c6c235b258f9dfc74fe7e277d39a48f
Caleb Evans <calebsevan@gmail.com>
2024-08-02 20:43:22 -0500

    link ui added for collapse menu

--- app/clocks/[userId]/collapse-open/page.tsx
+++ app/clocks/[userId]/collapse-open/page.tsx
@@ -1,23 +1,32 @@
 import React from "react";
 import Link from "next/link";
+import { routingDataArray } from "./routing-data";
+
+interface RoutingDataItem {
+  title: string;
+  url: string;
+}
 
 export default function Login({ params }) {
+  const routingData: RoutingDataItem[] = routingDataArray(params.userId);
   return (
     <div className="absolute w-full h-screen">
-      <Link className="absolute inset-0 cursor-default bg-neutral-950 bg-opacity-40 z-10" href={`/clocks/${params.userId}`}></Link>
-      <div
-        className="flex flex-col justify-around items-center p-4 pt-6 w-72 h-auto absolute rounded-lg border-4 border-accent-1 top-1/4 left-1/2 transform -translate-x-1/2 bg-bkg z-20"
-        id="authForm"
-      >
-        <h1 className="text-accent-1 text-4xl font-bold mb-4">Log in</h1>
-        <p className="mb-4">or</p>
-        <Link
-          href={`/clocks/${params.userId}/create-account`}
-          className="underline cursor-pointer mb-8"
-        >
-          create an account
-        </Link>
-        {/* <InputForm /> */}
+      <Link
+        className="absolute inset-0 cursor-default bg-neutral-950 bg-opacity-40 z-10"
+        href={`/clocks/${params.userId}`}
+      ></Link>
+      <div className="flex flex-col justify-around items-center p-4 pt-6 w-72 h-auto absolute rounded-lg border-4 border-accent-1 top-1/4 left-1/2 transform -translate-x-1/2 bg-bkg z-20">
+        {routingData.map((entry, idx) => {
+          return (
+            <Link
+              key={idx}
+              href={entry.url}
+              className="text-accent-1 text-2xl border-accent-2 text-center border-4 p-4 min-w-[90%] rounded-lg font-bold mb-4"
+            >
+              {entry.title}
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
---
+++ app/clocks/[userId]/collapse-open/routing-data.tsx
@@ -0,0 +1,17 @@
+interface RoutingDataItem {
+  title: string;
+  url: string;
+}
+
+export function routingDataArray(id: string) {
+  return [
+    {
+      title: "Login",
+      url: `/clocks/${id}/login`,
+    },
+    {
+      title: "Create Account",
+      url: `/clocks/${id}/create-account`,
+    },
+  ];
+}
