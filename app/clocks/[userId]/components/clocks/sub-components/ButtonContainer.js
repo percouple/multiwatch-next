b00a92c288aad3e6ae6f67c0632758ed456f0841
Caleb Evans <calebsevan@gmail.com>
2024-08-07 06:49:58 -0500

    new icons for play button

--- app/clocks/[userId]/components/clocks/sub-components/ButtonContainer.js
+++ app/clocks/[userId]/components/clocks/sub-components/ButtonContainer.js
@@ -1,21 +1,23 @@
 import React from "react";
+import StartClockIcon from '../../icons/StartClockIcon';
+import PauseClockIcon from '../../icons/PauseClockIcon';
 
 export default function ButtonContainer({ punchedIn, onPunchIn, onPunchOut }) {
   return (
-    <div className="flex justify-center my-2">
+    <div className="flex justify-center items-center">
       {punchedIn ? (
         <button
-          className={`flex justify-center rounded-lg p-2 w-36 cursor-pointer bg-accent-2 font-light transform transition-transform duration-300 ease-in-out hover:-translate-y-1 hover:shadow-black shadow-md`}
+          className={`flex justify-center rounded-full p-2 cursor-pointer bg-accent-2 font-light transform transition-transform duration-300 ease-in-out hover:-translate-y-1 hover:shadow-black shadow-md`}
           onClick={onPunchOut}
         >
-          Stop
+          <PauseClockIcon />
         </button>
       ) : (
         <button
-          className={`flex justify-center rounded-lg p-2 w-36 cursor-pointer bg-accent-1 font-light transform transition-transform duration-300 ease-in-out hover:-translate-y-1 hover:shadow-black shadow-md`}
+          className={`flex justify-center p-2 rounded-full cursor-pointer bg-accent-1 font-light transform transition-transform duration-300 ease-in-out hover:-translate-y-1 hover:shadow-black shadow-md`}
           onClick={onPunchIn}
         >
-          Start
+          <StartClockIcon/>
         </button>
       )}
     </div>
