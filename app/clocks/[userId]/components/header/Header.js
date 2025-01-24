0fa9c7ad5feeb16d0fab1959866d2203410d98a7
Caleb Evans <calebsevan@gmail.com>
2025-01-24 14:36:44 -0600

    Login button deletion from header

--- app/clocks/[userId]/components/header/Header.js
+++ app/clocks/[userId]/components/header/Header.js
@@ -1,5 +1,4 @@
 import CurrentTimeDisplay from "./sub-components/CurrentTime";
-import LoginButton from "./sub-components/LoginButton";
 import LogoutButton from "./sub-components/LogoutButton";
 import ThemeChangeButton from "./sub-components/ThemeChangeButton";
 import CollapsedMenu from "./sub-components/CollapsedMenu";
@@ -14,11 +13,8 @@ export default function Header({ theme, userId }) {
           <CollapsedMenu userId={userId} />
         </div>
         <div className="hidden md:flex items-center flex-1">
-          <ThemeChangeButton userId={userId} />
-          {/* {userId !== "default" ? ( */}
-          {/* <LogoutButton theme={theme} userId={userId} /> */}
-          {/* ) : ( */}
-          <LoginButton theme={theme} userId={userId} />
+          {/* <ThemeChangeButton userId={userId} /> */}
+          <LogoutButton theme={theme} userId={userId} />
           <CreateNewTimerButton theme={theme} userId={userId} />
         </div>
       </div>
--- app/clocks/[userId]/components/header/sub-components/LogoutButton.js
+++ app/clocks/[userId]/components/header/sub-components/LogoutButton.js
@@ -1,14 +1,15 @@
-import React from "react";
+"use client";
 
-export default function LogoutButton({ userId }) {
+import Link from "next/link";
 
+export default function LoginButton({ userId }) {
+  // REVOKE COOKIE IN THE FUTURE
   return (
-    <div 
-      id="dropdownButton"
-      // onClick={clickHandler}
-      className={`flex justify-around items-center cursor-pointer text-base py-2 px-4 border-3 rounded-md min-w-[4rem]`}
+    <Link
+      href={`/cold-login`}
+      className="text-center cursor-pointer mx-2 py-2 px-6 bg-bkg border-4 border-solid border-accent-1 rounded-md min-w-[4rem] transition-all hover:border-accent-2 duration-300 ease-in-out"
     >
       Logout
-    </div>
+    </Link>
   );
 }
