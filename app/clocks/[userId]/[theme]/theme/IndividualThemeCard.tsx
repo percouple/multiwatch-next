1aed26c227fcda854d30a5fa649463bffabda946
percouple <calebsevan@gmail.com>
2025-03-12 19:52:02 -0500

    theme change fix (again...)

--- app/clocks/[userId]/[theme]/theme/IndividualThemeCard.tsx
+++ app/clocks/[userId]/[theme]/theme/IndividualThemeCard.tsx
@@ -12,21 +12,21 @@ export default function ThemesDisplay({theme}) {
 
   // List of color variable namees for mapping in each box
   const colorNames = [
-    "skin-backgroundBase",
-    "skin-textBase",
-    "skin-accent-1",
-    "skin-accent-2",
+    "backgroundBase",
+    "accent-1",
+    "accent-2",
   ];
 
   return (
     <>
       <h2 onClick={() => clickHandler(theme)}>{theme}</h2>
-      <div className={`theme-${theme} flex justify-center items-center`}>
+      <div className={`theme-${theme} flex justify-center items-center border-2
+      border-gray-500 rounded-sm`}>
         {colorNames.map((colorName, idx) => {
           return (
             <div
               key={idx}
-              className={`bg-${colorName} w-[15px] h-[15px] rounded-full p-2 m-1 border-neutral-500 border-2`}
+              className={`bg-skin-${colorName} w-[15px] h-[15px] p-2`}
               onClick={() => clickHandler(theme)}
             ></div>
           );
